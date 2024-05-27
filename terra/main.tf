d04bf1a9960b7a7cc8c18326343e8698121bfc16
SySamba <sambasy837@gmail.com>
2024-05-27 18:13:09 +0200

    firt terraform

--- terra/main.tf
+++ terra/main.tf
@@ -1,5 +1,5 @@
 provider "kubernetes" {
   config_context_cluster = "minikube"
   // Remplacez le chemin par le chemin absolu vers votre fichier .kube/config
-  config_path = "C:\\Users\\HP\\.kube\\config"
+  config_path = "~/.kube/config"
 }
